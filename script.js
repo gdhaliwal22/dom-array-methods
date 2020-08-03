07303e0b8bc6806a9c846035b5f195e4b03cceda
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 12:21:26 -0700

    async function to get RandomUser

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const sort = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  console.log(newUser);
+}
