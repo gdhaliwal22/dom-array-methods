a76327d63f707146b190a832b141cb4edec1c53d
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 12:11:46 -0700

    create variables for elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionaires = document.getElementById("show-millionaires");
+const sort = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
