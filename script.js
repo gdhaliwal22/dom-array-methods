b11142a05d86c2548351fd3cdc0f6097c3bf02af
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 13:04:48 -0700

    calculate entire wealth via reduce method

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
 
 let data = [];
 getRandomUser();
@@ -48,6 +49,17 @@ function showMillionaires() {
     return item.money > 1000000;
   });
   updateDOM();
+}
+
+// Reduce to calculate total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  const wealthEl = document.createElement("div");
+
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 // Add new obj to data arr
